620d00933c2031c2c30bb18be70472fa26c126e1
EasyTrip24 <easytrip26@gmail.com>
2025-12-08 18:19:23 +0100

    checkpoint building

--- prisma/migrations/20251008195157_add_school_registration/migration.sql
+++
@@ -1,4 +0,0 @@
--- AlterTable
-ALTER TABLE "event_registrations" ADD COLUMN "cne" TEXT;
-ALTER TABLE "event_registrations" ADD COLUMN "school" TEXT;
-ALTER TABLE "event_registrations" ADD COLUMN "schoolYear" TEXT;
